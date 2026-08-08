d870ea0e5e60dbce604f633acdccc48dd9fd1043
Bennett <EthanBnntt@users.noreply.github.com>
2026-08-08 03:18:12 -0400

    chore: remove obsolete run_h100_qwen_multigpu script

--- scripts/run_h100_qwen_multigpu.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Thin wrapper: four-GPU Qwen-tokenized ~100M-parameter GDN run (see run_h100_multigpu.sh).
-exec env PROFILE=qwen bash "$(dirname "$0")/run_h100_multigpu.sh" "$@"
